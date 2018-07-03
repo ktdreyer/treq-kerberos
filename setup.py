f32835666921ff8abd33adef702ad66cf5906a0b
Ken Dreyer <kdreyer@redhat.com>
2018-07-03 13:37:59 -0600

    test python 3.7 in Travis CI

--- setup.py
+++ setup.py
@@ -133,6 +133,7 @@ setup(
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
     ],
     install_requires=[
         'kerberos',
