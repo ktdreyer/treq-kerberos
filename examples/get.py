7b6cb4add77e5cc709e164afdb74b212cd5df821
Ken Dreyer <kdreyer@redhat.com>
2019-02-06 12:26:57 -0700

    examples: use twisted.internet.task.react
    Simplify the example code with the react() method.

--- examples/get.py
+++ examples/get.py
@@ -1,24 +1,19 @@
 from treq_kerberos import TreqKerberosAuth
 import treq_kerberos
-from twisted.internet import defer, reactor
+from twisted.internet import defer
+from twisted.internet.task import react
 
 # Get a URL that requires Kerberos authentication.
 
 
 @defer.inlineCallbacks
-def example():
+def example(reactor):
     url = 'https://errata.devel.redhat.com/'
     auth = TreqKerberosAuth()
-    try:
-        response = yield treq_kerberos.get(url, auth=auth)
-        content = yield response.content()
-        print(content)
-    except Exception as e:
-        print(e)
+    response = yield treq_kerberos.get(url, auth=auth)
+    content = yield response.content()
+    print(content)
 
 
 if __name__ == '__main__':
-    d = example()
-    d.addCallback(lambda ign: reactor.stop())
-    d.addErrback(lambda ign: reactor.stop())
-    reactor.run()
+    react(example)
